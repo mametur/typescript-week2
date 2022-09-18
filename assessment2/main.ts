1b09620b6b5fbb56048451b32509148e18d0d506
Mame <mazad.contractor@libertyglobal.com>
2022-09-18 15:49:31 +0200

    assessment two

--- assessment2/main.ts
+++ assessment2/main.ts
@@ -1,6 +1,21 @@
-function sumOfMultiple(limit: any): any {
-    // Your code goes here
-    return 0;
-}
+
+const sumOfMultiple = (limit: number): number => {
+
+    const numberOne: number = 3
+    const numberTwo: number = 5;
+    let total: number = 0;
+
+    for (let index = 0; index < limit; index++) {
+    const resultOne = (index)%(numberOne);
+    const resultTwo = (index)%(numberTwo); 
+
+    if(resultOne === 0 || resultTwo === 0) {
+        total += index;
+    }
+    }
+
+    return total; 
+    
+} 
 
 module.exports = sumOfMultiple;
